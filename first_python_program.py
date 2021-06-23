0941adbf860a67808b2c67010e5556eb6b1b393c
Brian Harney <brian.r.harney@gmail.com>
2021-06-23 14:35:15 -0500

    Write fuction and while loop

--- first_python_program.py
+++ first_python_program.py
@@ -1,4 +1,18 @@
-day_hours = 24
-week_days = 7
+def sentence_maker(phrase):
+    iterrogatives = ("who", "what", "where", "when", "why", "how")
+    capitalized = phrase.capitalize()
+    if phrase.startswith(iterrogatives):
+        return "{}?".format(capitalized)
+    else:
+        return "{}.".format(capitalized)
 
-print(day_hours * week_days)
+
+paragraph = []
+while True:
+    sentence = input("Say something: ")
+    if sentence == "\end":
+        break
+    else:
+        paragraph.append(sentence_maker(sentence))
+
+print(" ".join(paragraph))
